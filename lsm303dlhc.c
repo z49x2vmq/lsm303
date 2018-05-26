90681aed3cb47795bb62b09b5263634bb76820db
Rae Kim <raekim@gmail.com>
2018-05-26 23:08:10 +0900

    blocking i2c function

--- lsm303dlhc.c
+++ lsm303dlhc.c
@@ -3,11 +3,11 @@
 
 void lsm303dlhc_init_la(I2C_HandleTypeDef *i2c) {
     uint8_t init[2][2] = {
-        {CTRL_REG1_A, (uint8_t)0x37},
-        {CTRL_REG4_A, (uint8_t)0x10}
+        {CTRL_REG1_A, ODR_400Hz | LPen | Xen | Yen | Zen},
+        {CTRL_REG4_A, BDU | FS_4G | HR}
     };
 
-    volatile HAL_StatusTypeDef ret;
+    HAL_StatusTypeDef ret;
 
     ret = HAL_I2C_Master_Transmit(i2c, LA_ADDRESS, init[0], 2, 1000);
 
@@ -28,7 +28,7 @@ void lsm303dlhc_init_mf(I2C_HandleTypeDef *i2c) {
         {MR_REG_M, (uint8_t)0x00}
     };
 
-    volatile HAL_StatusTypeDef ret;
+    HAL_StatusTypeDef ret;
 
     ret = HAL_I2C_Master_Transmit(i2c, MF_ADDRESS, init[0], 2, 1000);
     if (ret != HAL_OK) {
@@ -45,7 +45,7 @@ void lsm303dlhc_init_mf(I2C_HandleTypeDef *i2c) {
 void lsm303dlhc_read_la(I2C_HandleTypeDef *i2c, uint8_t *buf) {
     uint8_t reg = OUT_X_L_A | 0b10000000;
 
-    volatile HAL_StatusTypeDef ret;
+    HAL_StatusTypeDef ret;
 
     ret = HAL_I2C_Master_Sequential_Transmit_IT(i2c, LA_ADDRESS, &reg, 1, I2C_FIRST_FRAME);
     if (ret != HAL_OK) {
@@ -64,7 +64,7 @@ void lsm303dlhc_read_la(I2C_HandleTypeDef *i2c, uint8_t *buf) {
 void lsm303dlhc_read_mf(I2C_HandleTypeDef *i2c, uint8_t *buf) {
     uint8_t reg = OUT_X_H_M;
 
-    volatile HAL_StatusTypeDef ret;
+    HAL_StatusTypeDef ret;
 
     ret = HAL_I2C_Master_Sequential_Transmit_IT(i2c, MF_ADDRESS, &reg, 1, I2C_FIRST_FRAME);
     if (ret != HAL_OK) {
@@ -79,4 +79,35 @@ void lsm303dlhc_read_mf(I2C_HandleTypeDef *i2c, uint8_t *buf) {
     while(i2c->State != HAL_I2C_STATE_READY);
 }
 
+void lsm303dlhc_read_la_b(I2C_HandleTypeDef *i2c, uint8_t *buf) {
+    uint8_t reg = OUT_X_L_A | 0b10000000;
+
+    HAL_StatusTypeDef ret;
+
+    ret = HAL_I2C_Master_Transmit(i2c, LA_ADDRESS, &reg, 1, 1000);
+    if (ret != HAL_OK) {
+        return;
+    }
+
+    ret = HAL_I2C_Master_Receive(i2c, LA_ADDRESS, buf, 6, 1000);
+    if (ret != HAL_OK) {
+        return;
+    }
+}
+
+void lsm303dlhc_read_mf_b(I2C_HandleTypeDef *i2c, uint8_t *buf) {
+    uint8_t reg = OUT_X_H_M;
+
+    HAL_StatusTypeDef ret;
+
+    ret = HAL_I2C_Master_Transmit(i2c, MF_ADDRESS, &reg, 1, 1000);
+    if (ret != HAL_OK) {
+        return;
+    }
+
+    ret = HAL_I2C_Master_Receive(i2c, MF_ADDRESS, buf, 6, 1000);
+    if (ret != HAL_OK) {
+        return;
+    }
+}
 // vim: ts=4 sw=4 expandtab
